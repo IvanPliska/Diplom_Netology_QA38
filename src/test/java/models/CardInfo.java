107b0abdad09db33c9172623b3e8564ad8b72d1d
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-12-21 14:34:08 +0300

    Add models

---
+++ src/test/java/models/CardInfo.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardInfo {
+    String number;
+    String month;
+    String year;
+    String holder;
+    String cvc;
+}
